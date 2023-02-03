928828d37945dc92b2d1298bf93946d2a5abe37e
igitonga <iangitonga47@gmail.com>
2023-02-03 18:11:12 +0300

    hover effects

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Logo from "../../images/logo.svg"
 import "./navbar.scss"
+import ArrowDown from "../../images/icon-arrow-down.svg"
+
 const Navbar = () => {
     return(
         <div className="nav-main">
             <div className="nav-links">
                 <img className="logo" src={Logo} alt="" />
                 <ul>
-                    <li>Feature</li>
-                    <li>Company</li>
+                    <li>Feature <img src={ArrowDown} alt="" /></li>
+                    <li>Company <img src={ArrowDown} alt="" /></li>
                     <li>Careers</li>
                     <li>About</li>
                 </ul>
