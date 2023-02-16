deaeebf56192b75511df155b58ba77c2429f3729
igitonga <iangitonga47@gmail.com>
2023-02-16 17:16:47 +0300

    styled dropdowns

--- src/components/Dropdowns/FeatureDropdown.jsx/FeatureDropdown.jsx
+++ src/components/Dropdowns/FeatureDropdown.jsx/FeatureDropdown.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./featureDropdown.scss"
+import Todo from "../../../images/icon-todo.svg"
+import Planning from "../../../images/icon-planning.svg"
+import Calendar from "../../../images/icon-calendar.svg"
+import Reminder from "../../../images/icon-reminders.svg"
 
 const FeatureDropdown = () => {
     return(
         <div className="cont">
             <ul>
-                <li>Todo list</li>
-                <li>Calendar</li>
-                <li>Reminders</li>
-                <li>Planning</li>
+                <li><img src={Todo} alt="todo icon" /> Todo list</li>
+                <li><img src={Calendar} alt="Calendar icon" /> Calendar</li>
+                <li><img src={Reminder} alt="Reminders icon" /> Reminders</li>
+                <li><img src={Planning} alt="Planning icon" /> Planning</li>
             </ul>
         </div>
     )
